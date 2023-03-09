033f963380dcdf1412601e2048d88155689a82b1
Michael Parry <michael.parry.87@gmail.com>
2023-03-09 12:47:58 +0100

    feat(netatmo): remove Pressure from outdoor module

--- apps/netatmo/src/app/shared/constants/measure-type-by-module-type.ts
+++ apps/netatmo/src/app/shared/constants/measure-type-by-module-type.ts
@@ -3,7 +3,7 @@ import { ModuleType } from '../api/enums/module-type';
 
 export const MEASURE_TYPE_BY_MODULE_TYPE: Record<ModuleType, MeasureType[]> = {
   [ModuleType.WeatherStation]: [MeasureType.Temperature, MeasureType.CO2, MeasureType.Pressure, MeasureType.Humidity, MeasureType.Noise],
-  [ModuleType.OutdoorModule]: [MeasureType.Temperature, MeasureType.Humidity, MeasureType.Pressure],
+  [ModuleType.OutdoorModule]: [MeasureType.Temperature, MeasureType.Humidity],
   [ModuleType.IndoorModule]: [MeasureType.Temperature, MeasureType.Humidity, MeasureType.CO2],
   [ModuleType.AnemometerModule]: [MeasureType.WindStrength],
   [ModuleType.RainGaugeModule]: [MeasureType.Rain],
