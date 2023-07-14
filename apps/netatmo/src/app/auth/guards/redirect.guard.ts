564d84aad0bc3e61dc83e20bb5a8b4197e8d7f5f
Michael Parry <michael.parry.87@gmail.com>
2023-07-14 20:03:37 +0200

    feat(netatmo): auto refresh token

--- apps/netatmo/src/app/auth/guards/redirect.guard.ts
+++ apps/netatmo/src/app/auth/guards/redirect.guard.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Router, UrlTree } from '@angular/router';
 import { OAuthService } from 'angular-oauth2-oidc';
-import { from, map, Observable } from 'rxjs';
+import { from, map, Observable, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class RedirectGuard {
   constructor(private oauthService: OAuthService, private router: Router) {}
 
   canActivate(): Observable<UrlTree> {
-    return from(this.oauthService.tryLogin()).pipe(map(() => this.router.createUrlTree([])));
+    return from(this.oauthService.tryLogin()).pipe(
+      tap(() => this.oauthService.setupAutomaticSilentRefresh()),
+      map(() => this.router.createUrlTree([]))
+    );
   }
 }
